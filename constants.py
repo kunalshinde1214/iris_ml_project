60e6ace07b59143c165b200acfbe892f29a4a175
Kunal Shinde <145975183+kunalshinde1214@users.noreply.github.com>
2025-08-10 17:40:50 +0530

    DATA

---
+++ constants.py
@@ -0,0 +1,14 @@
+#all folder and filepaths and train size
+from pathlib import Path
+
+#Data Directory
+DATA_DIR = Path("data")
+DATA_FILE = DATA_DIR / "iris.csv"
+
+#Model Directory
+MODEL_DIR = Path("models")
+MODEL_FILE = MODEL_DIR / "iris_model.joblib"
+
+#Training_Configation
+TRAIN_SIZE = 0.33
+RANDOM_STATE = 21
